c28f7f4d94d4bbb7d13c2f313521cab1451c93b0
mneddi <91653034+mneddi@users.noreply.github.com>
2021-10-18 03:54:27 +0200

    SVGElement Class - Constructor Create ElementNS

--- js/sight.js
+++ js/sight.js
@@ -2,5 +2,8 @@ class SVGElement {
     constructor(type) {
         this.type = type;
         this.namespace = 'http://www.w3.org/2000/svg';
+        this.node = document.createElementNS(this.namespace, this.type);
+
+        return this;
     }
 }
